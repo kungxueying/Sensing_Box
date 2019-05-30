317531820b1096ba8d2ac8d58c512a8b44e389f2
hihisharon97 <50620868+hihisharon97@users.noreply.github.com>
2019-05-30 10:42:04 +0800

    30/5/19 10:40

---
+++ app/src/main/java/com/example/sensingbox/upload_success.java
@@ -0,0 +1,13 @@
+package com.example.sensingbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class upload_success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_success);
+    }
+}
