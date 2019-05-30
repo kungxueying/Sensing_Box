e199e5ebfb4116dabc2f455e84e7b5f12560f3a2
hihisharon97 <50620868+hihisharon97@users.noreply.github.com>
2019-05-30 11:41:24 +0800

    30/5/19 11:41

---
+++ app/src/main/java/com/example/sensingbox/first_page.java
@@ -0,0 +1,13 @@
+package com.example.sensingbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class first_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
