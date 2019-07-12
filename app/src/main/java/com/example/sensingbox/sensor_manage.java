34d6cea4bd5b3b4c0ff0a7367f05b847717f3858
littlestone1225 <littlestone1225@yahoo.com.tw>
2019-07-12 08:07:36 +0800

    test

--- app/src/main/java/com/example/sensingbox/sensor_manage.java
+++ app/src/main/java/com/example/sensingbox/sensor_manage.java
@@ -1,28 +1,18 @@
 package com.example.sensingbox;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.app.Activity;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class sensor_select extends AppCompatActivity {
+public class sensor_manage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensor_select);
-
-    }
-
-    public void goTo_Sensor_Manage (View view){
-        Intent intent = new Intent (this, sensor_manage.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_sensor_manage);
     }
 
 }
