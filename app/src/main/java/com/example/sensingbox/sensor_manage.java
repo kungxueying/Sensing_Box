7f24abc637184a007df634fe18083eb3aedd093b
littlestone1225 <littlestone1225@yahoo.com.tw>
2019-07-18 00:20:53 +0800

    Update sensor_manage.java

--- app/src/main/java/com/example/sensingbox/sensor_manage.java
+++ app/src/main/java/com/example/sensingbox/sensor_manage.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -55,10 +56,11 @@ public class sensor_manage extends AppCompatActivity {
     private final static int CONNECTING_STATUS = 3;
     // used in bluetooth handler to identify message status
     private String _recieveData = "";
+    private String _sendCMD = "";
 
     private Handler mHandler;
     // Our main handler that will receive callback notifications
-    //private ConnectedThread mConnectedThread;
+    private ConnectedThread mConnectedThread;
     // bluetooth background worker thread to send and receive data
     private BluetoothSocket mBTSocket = null;
     // bi-directional client-to-client data path
@@ -132,6 +134,33 @@ public class sensor_manage extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]
                     {Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
 
+        mHandler = new Handler(){
+            public void handleMessage(android.os.Message msg){
+                if(msg.what == MESSAGE_READ){ //收到MESSAGE_READ 開始接收資料
+                    String readMessage = null;
+                    try {
+                        readMessage = new String((byte[]) msg.obj, "UTF-8");
+                        readMessage =  readMessage.substring(0,1);
+                        //取得傳過來字串的第一個字元，其餘為雜訊
+                        _recieveData += readMessage; //拼湊每次收到的字元成字串
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    textview.append(readMessage); //將收到的字串呈現在畫面上
+
+                }
+
+                if(msg.what == CONNECTING_STATUS){
+                    //收到CONNECTING_STATUS 顯示以下訊息
+                    if(msg.arg1 == 1)
+                        textview.append("Connected to Device: " + (String)(msg.obj));
+                    else
+                        textview.append("Connection Failed");
+                }
+            }
+        };
+
+
         if (mBTArrayAdapter == null) {
             // Device does not support Bluetooth
             textview.append("Status: Bluetooth not found");
@@ -141,16 +170,23 @@ public class sensor_manage extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     _recieveData = ""; //清除上次收到的資料
-
+                    _sendCMD = "1,"+input_command.getText()+ "\n";
                     textview.append("click send_command \n >");
-                    textview.append(input_command.getText()+ "\n");
+                    textview.append("cmd: 1,"+input_command.getText()+ "\n");
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write(_sendCMD);
+                    //傳送將輸入的資料出去
                 }
             });
             get_sensor.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     _recieveData = ""; //清除上次收到的資料
-                    textview.append( "click get_sensor\n");
+                    _sendCMD = "2,0\n";
+                    textview.append( "click get_sensor(all)\n");
+                    textview.append("cmd: "+_sendCMD);
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write(_sendCMD);
                 }
             });
 
@@ -158,7 +194,11 @@ public class sensor_manage extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     _recieveData = ""; //清除上次收到的資料
+                    _sendCMD = "3,1,23,run,light,1,hero3748,/data/datalog.txt\n";
                     textview.append(  "click set_sensor\n");
+                    textview.append("cmd: "+_sendCMD);
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write(_sendCMD);
                 }
             });
 
@@ -166,7 +206,11 @@ public class sensor_manage extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     _recieveData = ""; //清除上次收到的資料
+                    _sendCMD = "4,1\n";
                     textview.append(  "click del_sensor\n");
+                    textview.append("cmd: "+_sendCMD);
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write(_sendCMD);
                 }
             });
 
@@ -174,7 +218,11 @@ public class sensor_manage extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     _recieveData = ""; //清除上次收到的資料
+                    _sendCMD = "5,1,23,run,light,1,hero3748,/data(new)/datalog.txt\n";
                     textview.append( "click edit_sensor\n");
+                    textview.append("cmd: "+_sendCMD);
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write(_sendCMD);
                 }
             });
 
@@ -182,7 +230,11 @@ public class sensor_manage extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     _recieveData = ""; //清除上次收到的資料
+                    _sendCMD = "6\n";
                     textview.append(  "click upload_data\n");
+                    textview.append("cmd: "+_sendCMD);
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write(_sendCMD);
                 }
             });
 
@@ -224,8 +276,8 @@ public class sensor_manage extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if(BluetoothDevice.ACTION_FOUND.equals(action)){
-                BluetoothDevice device =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
                 // add the name to the list
                 mBTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 mBTArrayAdapter.notifyDataSetChanged();
@@ -242,24 +294,137 @@ public class sensor_manage extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Show Paired Devices", Toast.LENGTH_SHORT).show();
         }
         else
-            Toast.makeText(getApplicationContext(), "Bluetooth not on",
-                    Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
     }
 
     private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView<?> av, View v, int position, long id) {
-
-            textview.append(  "You have clicked : " + datas[position] +"\n");
+            textview.append("You have clicked : " + datas[position] +"\n");
             dialog.dismiss();
 
-            /*
             if(!mBTAdapter.isEnabled()) {
-                Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "Bluetooth not on",
+                        Toast.LENGTH_SHORT).show();
                 return;
             }
-            */
+
+            textview.append("Connecting...\n");
+            // Get the device MAC address, which is the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            final String address = info.substring(info.length() - 17);
+            final String name = info.substring(0,info.length() - 17);
+
+            // Spawn a new thread to avoid blocking the GUI one
+            new Thread()
+            {
+                public void run() {
+                    boolean fail = false;
+                    //取得裝置MAC找到連接的藍芽裝置
+                    BluetoothDevice device = mBTAdapter.getRemoteDevice(address);
+
+                    try {
+                        mBTSocket = createBluetoothSocket(device);
+                        //建立藍芽socket
+                    } catch (IOException e) {
+                        fail = true;
+                        Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                    }
+                    // Establish the Bluetooth socket connection.
+                    try {
+                        mBTSocket.connect(); //建立藍芽連線
+                    } catch (IOException e) {
+                        try {
+                            fail = true;
+                            mBTSocket.close(); //關閉socket
+                            //開啟執行緒 顯示訊息
+                            mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                    .sendToTarget();
+                        } catch (IOException e2) {
+                            //insert code to deal with this
+                            Toast.makeText(getBaseContext(), "Socket creation failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    if(fail == false) {
+                        //開啟執行緒用於傳輸及接收資料
+                        mConnectedThread = new ConnectedThread(mBTSocket);
+                        mConnectedThread.start();
+                        //開啟新執行緒顯示連接裝置名稱
+                        mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
+                                .sendToTarget();
+                    }
+                }
+            }.start();
         }
     };
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connection with BT device using UUID
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.available();
+                    if(bytes != 0) {
+                        SystemClock.sleep(100);
+                        //pause and wait for rest of data
+                        bytes = mmInStream.available();
+                        // how many bytes are ready to be read?
+                        bytes = mmInStream.read(buffer, 0, bytes);
+                        // record how many bytes we actually read
+                        mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                                .sendToTarget(); // Send the obtained bytes to the UI activity
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            byte[] bytes = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
 }
 
 
