db41e29e8d3db646b927559f0667680cd3aac68f
hihisharon97 <50620868+hihisharon97@users.noreply.github.com>
2019-08-15 15:17:19 +0800

    190815 15:17 (User Info)

--- app/src/main/java/com/example/sensingbox/User_info.java
+++ app/src/main/java/com/example/sensingbox/User_info.java
@@ -2,12 +2,43 @@ package com.example.sensingbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class User_info extends AppCompatActivity {
+
+    ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Username:",
+                "Box Number:",
+                "Email:",
+                "Box Info:",
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
     }
+
 }
