b86abb36933f2207b32ef04895b9e0d8964901ac
Fangi <icedrip7@gmail.com>
2019-08-19 04:15:22 +0800

    local db insert delete function success

--- app/src/main/java/com/example/sensingbox/DB_MyDBHelper.java
+++ app/src/main/java/com/example/sensingbox/DB_MyDBHelper.java
@@ -3,25 +3,25 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
-public class MyDBHelper extends SQLiteOpenHelper {
+public class DB_MyDBHelper extends SQLiteOpenHelper {
 
     // 資料庫名稱
-    public static final String DATABASE_NAME = "mydata.db";
+    public static final String DATABASE_NAME = "mydata2.db";
     // 資料庫版本，資料結構改變的時候要更改這個數字，通常是加一
     public static final int VERSION = 1;
     // 資料庫物件，固定的欄位變數
     private static SQLiteDatabase database;
 
     // 建構子，在一般的應用都不需要修改
-    public MyDBHelper(Context context, String name, CursorFactory factory,
-                      int version) {
+    public DB_MyDBHelper(Context context, String name, CursorFactory factory,
+                         int version) {
         super(context, name, factory, version);
     }
 
     // 需要資料庫的元件呼叫這個方法，這個方法在一般的應用都不需要修改
     public static SQLiteDatabase getDatabase(Context context) {
         if (database == null || !database.isOpen()) {
-            database = new MyDBHelper(context, DATABASE_NAME,
+            database = new DB_MyDBHelper(context, DATABASE_NAME,
                     null, VERSION).getWritableDatabase();
         }
 
@@ -31,14 +31,13 @@ public class MyDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // 建立應用程式需要的表格
-        // 待會再回來完成它
+        db.execSQL(DB_itemDAO.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // 刪除原有的表格
-        // 待會再回來完成它
-
+        db.execSQL("DROP TABLE IF EXISTS " + DB_itemDAO.TABLE_NAME);
         // 呼叫onCreate建立新版的表格
         onCreate(db);
     }
--- app/src/main/java/com/example/sensingbox/MainActivity.java
+++ app/src/main/java/com/example/sensingbox/MainActivity.java
@@ -8,21 +8,101 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
     DS_user newuser = new DS_user();
     DS_dataset newdata = new DS_dataset();
     DS_sensorall sensor = new DS_sensorall();
     firebase_upload fb = new firebase_upload();
 
+    private DB_itemDAO itemDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //setNewuser(newuser);
         //setNewsensor(sensor);
         //setNewdata(newdata);
         //setFb(fb);
+        System.out.println("sqllite test");
+        //sqllite test
+        //dbHelper = new DB_MyDBHelper(this);
+        //db = dbHelper.getWritableDatabase(); // 開啟資料庫
+        // 建立資料庫物件
+        itemDAO = new DB_itemDAO(getApplicationContext());
+        System.out.println("建立資料庫物件成功");
+        // 如果資料庫是空的，就建立一些範例資料
+        // 這是為了方便測試用的，完成應用程式以後可以拿掉
+        //if (itemDAO.getCount() == 0) {
+            System.out.println("資料庫是空的");
+            itemDAO.sample();
+       // }
+
+        System.out.println("新增資料成功");
+        List<DS_dataset> items = new ArrayList<>();
+        // 取得所有記事資料
+        items = itemDAO.getAll();
+
+        //System.out.println(items);
+        System.out.println("讀取資料成功");
+        itemDAO.close();
+
     }
+    /*
+    @Override
+    protected void onStop() {
+        super.onStop();
+        db.close(); // 關閉資料庫
+    }
+    // Button元件的事件處理 - 插入記錄
+    public void btn1_Click(View view) {
+        long id;
+        ContentValues cv = new ContentValues();
+        cv.put("_id", Integer.parseInt(txtID.getText().toString()));
+        cv.put("name", txtName.getText().toString());
+        cv.put("grade", Double.parseDouble(txtGrade.getText().toString()));
+        id = db.insert(DATABASE_TABLE, null, cv);
+        output.setText("新增記錄成功: " + id);
+        command.setText("INSERT INTO Student _id,name,grade
+                Values"+txtID.getText().toString()+","+txtName.getText().toString()
+                        +","+txtGrade.getText().toString());
+    }
+    // 查詢所有記錄
+    public void btn4_Click(View view) {
+        // 查詢整個資料表
+        SqlQuery("SELECT * FROM " + DATABASE_TABLE);
+        command.setText("SELECT * FROM  Student");
+    }
+    // 執行SQL查詢
+    public void SqlQuery(String sql) {
+        String[] colNames;
+        String str = "";
+        Cursor c = db.rawQuery(sql, null);
+        colNames = c.getColumnNames();
+        // 顯示欄位名稱
+        for (int i = 0; i < colNames.length; i++)
+            str += colNames[i] + "\t\t";
+        str += "\n";
+        c.moveToFirst();  // 第1筆
+        // 顯示欄位值
+        for (int i = 0; i < c.getCount(); i++) {
+            str += c.getString(0) + "\t\t";
+            str += c.getString(1) + "\t\t";
+            str += c.getString(2) + "\n";
+            c.moveToNext();  // 下一筆
+        }
+        output.setText(str.toString());
+    }
+    */
 
     public void goTo_Login (View view){
         Intent intent = new Intent (this, main_screen.class);
