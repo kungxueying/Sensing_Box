43cb588247c72862f2a7d14724d0882456ee87db
Fangi <icedrip7@gmail.com>
2019-08-21 14:33:00 +0800

    Update DS_dataset.java

--- app/src/main/java/com/example/sensingbox/DS_dataset.java
+++ app/src/main/java/com/example/sensingbox/DS_dataset.java
@@ -10,4 +10,21 @@ public class DS_dataset {
     public String boxID;
     public String x;
     public String y;
+    public DS_dataset(){
+
+    }
+
+    public DS_dataset(String id,String BOXID,String DATA,String USERID,String LOCATE,String SENSOR,String TIME
+            ,String X,String Y){
+        this.id = id;
+        this.boxID = BOXID;
+        this.data = DATA;
+        this.userID = USERID;
+        this.locate = LOCATE;
+        this.sensor = SENSOR;
+        this.time = TIME;
+        this.x = X;
+        this.y = Y;
+
+    }
 }
