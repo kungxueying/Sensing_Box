a4d6364326e611c2efcce6f8a4d953d918674b66
hihisharon97 <50620868+hihisharon97@users.noreply.github.com>
2019-09-24 17:25:49 +0800

    190924 17:25 (spinner)

--- app/src/main/java/com/example/sensingbox/add_edit_sensor.java
+++ app/src/main/java/com/example/sensingbox/add_edit_sensor.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class add_edit_sensor extends AppCompatActivity {
 
     sensor sensor1=new sensor();
     TextView sensor_name;
-    EditText Cycle;
-    EditText Status;
+    Spinner spinner1;
+    Spinner spinner2;
+    String cycle_temp;
+    String status_temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +32,44 @@ public class add_edit_sensor extends AppCompatActivity {
         sensor_name = (TextView) findViewById(R.id.textView2);
         sensor_name.setText(sensor1.getSensorName());
 
-        Cycle = (EditText) findViewById(R.id.editText);
-        Status = (EditText) findViewById(R.id.editText2);
+        //spinner
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        final String[] cycle = {"5", "10", "20", "30", "60"};
+        ArrayAdapter<String> cycleList = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                cycle);spinner1.setAdapter(cycleList);
+
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        final String[] status = {"Run", "Pause"};
+        ArrayAdapter<String> statusList = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                status);spinner2.setAdapter(statusList);
+
+        cycle_temp=Integer.toString(sensor1.getCycle());
+        status_temp=sensor1.getStatus();
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cycle_temp = cycle[position];
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                status_temp = status[position];
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
 
     public void ok (View view){
-        if(!Cycle.getText().toString().isEmpty())sensor1.setCycle(Integer.parseInt(Cycle.getText().toString()));
-        if(!Status.getText().toString().isEmpty())sensor1.setStatus(Status.getText().toString());
+        sensor1.setCycle(Integer.parseInt(cycle_temp));
+        sensor1.setStatus(status_temp);
 
         Intent intent = new Intent (this, sensor_select.class);
         intent.putExtra("data",sensor1);
